fee84c035bc465bd0eb4261db97063fd4b530e52
ReshmaRL <125993234+ReshmaRL@users.noreply.github.com>
2023-02-22 20:38:55 +0530

    Add files via upload

---
+++ Driving.java
@@ -0,0 +1,12 @@
+class Driving{
+public static void main(String args[]){
+int age=18;
+if(age>0){
+System.out.println("Candidate is Eligible");
+}else if(age<0){
+System.out.println("Candidate is not Eligible");
+}else{
+System.out.println("Error");
+}
+}
+}
